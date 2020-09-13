2d6b66c47b8f01b35e304236a7b5a79af404e7d3
Olutomilayo Amazing-Grace <olutomilayodolapo@gmail.com>
2020-09-13 19:40:54 +0100

    Update password with hashing

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,6 +1,7 @@
+import { AuthCredentialsDto } from "./dtos/auth-credentials.dto";
+import * as bcrypt from 'bcrypt';
 import { ConflictException, InternalServerErrorException } from "@nestjs/common";
 import { EntityRepository, Repository } from "typeorm";
-import { AuthCredentialsDto } from "./dtos/auth-credentials.dto";
 import { User } from "./user.entity";
 
 @EntityRepository(User)
@@ -10,7 +11,8 @@ export class UserRepository extends Repository<User> {
 
         const user = new User();
         user.username = username;
-        user.password = password;
+        user.salt = await bcrypt.genSalt()
+        user.password = await this.hashPassword(password, user.salt);
 
         try {
             await user.save()
@@ -21,5 +23,9 @@ export class UserRepository extends Repository<User> {
                 throw new InternalServerErrorException;
             }
         }
+    }
+
+    private async hashPassword(password: string, salt: string): Promise<string> {
+        return bcrypt.hash(password, salt);
     }
 }
